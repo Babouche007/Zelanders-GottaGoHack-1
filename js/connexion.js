ca4a0418c1a89b689562d0e0d2e4c9b6d85edd89
julienvne <77295710+julienvne@users.noreply.github.com>
2021-05-22 01:11:02 +0200

    fixed bug

---
+++ js/connexion.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+//const navigation = document.querySelector('nav');
+
+//Animation input
+
+//const vaidationInput = document.querySelector('form-element');
+
+//validationIput.addEventListener('form-element', (e) => {
+//    if(e.target.value)
+//})
