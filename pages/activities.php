d61544b287b68a637b22de70b7208de8f6713d14
Babouche <bat.perrier@free.fr>
2021-05-22 04:33:02 +0200

    Now we can see the author of the post

--- pages/activities.php
+++ pages/activities.php
@@ -29,41 +29,48 @@ if(isset($_POST['create-activity'])){
         echo '<p class="error">Déscription requise</p>';
     }
     else{
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-        $tags = $_POST['tags'];
-        $idNotFound = true;
-        while($idNotFound){
-            $idNotFound = false;
-            $id = md5(random_bytes(10));
-            $query = $db->prepare("SELECT * FROM activities WHERE id=:id");
-            $query->bindParam("id", $id, PDO::PARAM_STR);
+        $author_id = $_SESSION['user_id'];
+        if($author_id){
+            $title = $_POST['title'];
+            $description = $_POST['description'];
+            $tags = $_POST['tags'];
+            $idNotFound = true;
+            while($idNotFound){
+                $idNotFound = false;
+                $id = md5(random_bytes(10));
+                $query = $db->prepare("SELECT * FROM activities WHERE id=:id");
+                $query->bindParam("id", $id, PDO::PARAM_STR);
+                $query->execute();
+                if ($query->rowCount() > 0) {
+                    $idFound = true;
+                }
+            }
+            $info = "" ;
+            $query = $db->prepare("SELECT * FROM activities WHERE title=:title");
+            $query->bindParam("title", $title, PDO::PARAM_STR);
             $query->execute();
             if ($query->rowCount() > 0) {
-                $idFound = true;
+                echo '<p class="error">Activité déjà créée</p>';
+            }
+            if ($query->rowCount() == 0){
+                $query = $db->prepare("INSERT INTO activities(id,author_id,title,tags,info,description) 
+        VALUES (:id,:author_id,:title,:tags,:info,:description)");
+                $query->bindParam("id", $id, PDO::PARAM_STR);
+                $query->bindParam("author_id", $author_id, PDO::PARAM_STR);
+                $query->bindParam("title", $title, PDO::PARAM_STR);
+                $query->bindParam("tags",$tags);
+                $query->bindParam("info", $info, PDO::PARAM_STR);
+                $query->bindParam("description", $description, PDO::PARAM_STR);
+                $result = $query->execute();
+                if ($result) {
+                    echo '<p class="success">Activité créée avec succés</p>';
+                } else {
+                    echo '<p class="error">Quelque chose n&apos;a pas foncionné</p>';
+                }
             }
         }
-        $info = "" ;
-        $query = $db->prepare("SELECT * FROM activities WHERE title=:title");
-        $query->bindParam("title", $title, PDO::PARAM_STR);
-        $query->execute();
-        if ($query->rowCount() > 0) {
-            echo '<p class="error">Activité déjà créée</p>';
-        }
-        if ($query->rowCount() == 0){
-            $query = $db->prepare("INSERT INTO activities(id,title,tags,info,description) 
-        VALUES (:id,:title,:tags,:info,:description)");
-            $query->bindParam("id", $id, PDO::PARAM_STR);
-            $query->bindParam("title", $title, PDO::PARAM_STR);
-            $query->bindParam("tags",$tags);
-            $query->bindParam("info", $info, PDO::PARAM_STR);
-            $query->bindParam("description", $description, PDO::PARAM_STR);
-            $result = $query->execute();
-            if ($result) {
-                echo '<p class="success">Activité créée avec succés</p>';
-            } else {
-                echo '<p class="error">Quelque chose n&apos;a pas foncionné</p>';
-            }
+        else{
+            echo "Voulez vous vous connecter?";
         }
     }
 }
@@ -73,6 +80,12 @@ while($activity = $query->fetch()){
     echo '<p class="separator">---------------------------------</p>';
     echo "Titre : " . $activity['title'] ?><br/><?php
     echo " Description : " . $activity['description'];?><br/><?php
+    $query = $db->prepare("SELECT * FROM users WHERE id=:id");
+    $query->bindParam("id", $_SESSION['user_id'], PDO::PARAM_STR);
+    $query->execute();
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+    echo "Author : " .$result['username']; ?><br/><?php
+
 }
 
 ?>
