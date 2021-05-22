f72e45ab0380023bbf062b8d626a5f5173b8b319
Babouche <bat.perrier@free.fr>
2021-05-22 03:14:02 +0200

    adding the list for categories

--- pages/activities.php
+++ pages/activities.php
@@ -6,10 +6,18 @@
 </head>
 <body>
 
-<form method="post">
+<form method="post" enctype="multipart/form-data">
     <input type="text" name="title" placeholder="Title"><br/>
     <input type="text" name="description" placeholder="Description"><br/>
-    <button type="submit" name="create-activity" value="create-activity">Cr&eacute;er</button>
+    <ul>
+        <li><button type="submit" name="videoGame" value="videoGame">Jeux Vidéos</button></li>
+        <li><button type="submit" name="music" value="music">Musique</button></li>
+        <li><button type="submit" name="sport" value="sport">Sport</button></li>
+        <li><button type="submit" name="discution" value="discution">Discussion</button></li>
+        <li><button type="submit" name="boardGame" value="boardGame">Jeux de sociétés</button></li>
+        <li><button type="submit" name="cinema" value="cinema">Cinéma</button></li>
+    </ul>
+    <button type="submit" name="create-activity" value="create-activity">Créer</button>
 </form>
 </body>
 </html>
@@ -20,33 +28,55 @@ include '../config.php';
 global $db;
 
 if(isset($_POST['create-activity'])){
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $info = "" ;
-    $query = $db->prepare("SELECT * FROM activities WHERE title=:title");
-    $query->bindParam("title", $title, PDO::PARAM_STR);
-    $query->execute();
-    if ($query->rowCount() > 0) {
-        echo '<p class="error">Activite deja cree</p>';
+    if (empty($_POST["title"])) {
+        echo '<p class="error">Titre requis</p>';
     }
-    if ($query->rowCount() == 0){
-        $query = $db->prepare("INSERT INTO activities(title,info,description) 
-        VALUES (:title,:info,:description)");
+    elseif (empty($_POST["description"])){
+        echo '<p class="error">Déscription requise</p>';
+    }
+    else{
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $idNotFound = true;
+        while($idNotFound){
+            $idNotFound = false;
+                $id = md5(random_bytes(10));
+            $query = $db->prepare("SELECT * FROM activities WHERE id=:id");
+            $query->bindParam("id", $id, PDO::PARAM_STR);
+            $query->execute();
+            if ($query->rowCount() > 0) {
+                $idFound = true;
+            }
+        }
+        $info = "" ;
+        $query = $db->prepare("SELECT * FROM activities WHERE title=:title");
         $query->bindParam("title", $title, PDO::PARAM_STR);
-        $query->bindParam("info", $info, PDO::PARAM_STR);
-        $query->bindParam("description", $description, PDO::PARAM_STR);
-        $result = $query->execute();
-        if ($result) {
-            echo '<p class="success">Activit&eacute; cr&eacute;e avec succ&eacute;s</p>';
-        } else {
-            echo '<p class="error">Quelque chose n&apos;a pas foncionn&eacute;</p>';
+        $query->execute();
+        if ($query->rowCount() > 0) {
+            echo '<p class="error">Activité déjà créée</p>';
+        }
+        if ($query->rowCount() == 0){
+            $query = $db->prepare("INSERT INTO activities(id,title,category,info,description) 
+        VALUES (:id,:title,:info,:description)");
+            $query->bindParam("id", $id, PDO::PARAM_STR);
+            $query->bindParam("title", $title, PDO::PARAM_STR);
+            $query->bindParam("info", $info, PDO::PARAM_STR);
+            $query->bindParam("description", $description, PDO::PARAM_STR);
+            $result = $query->execute();
+            if ($result) {
+                echo '<p class="success">Activité créée avec succés</p>';
+            } else {
+                echo '<p class="error">Quelque chose n&apos;a pas foncionné</p>';
+            }
         }
     }
 }
 
 $query = $db->query("SELECT * FROM activities ");
 while($activity = $query->fetch()){
-    echo "Titre : " . $activity['title'] .  " Description : " . $activity['description'];?><br/><?php
+    echo '<p class="separator">---------------------------------</p>';
+    echo "Titre : " . $activity['title'] ?><br/><?php
+    echo " Description : " . $activity['description'];?><br/><?php
 }
 
 ?>
