46538826a4fcaa802f51f15e3da2ef1bcae744cf
julienvne <77295710+julienvne@users.noreply.github.com>
2021-05-23 08:12:49 +0200

    tableau

--- pages/liste.php
+++ pages/liste.php
@@ -6,19 +6,20 @@
 
 <html lang="fr">
 <head>
-    <link href="css\Accueil.css" rel="stylesheet" type="text/css" />
+    <link href="..\css\tableau.css" rel="stylesheet" type="text/css" />
     <meta charset="utf-8" />
     <title>Activites</title>
     <link rel="shortcut icon" type="image/x-icon" href="res/img/logo.png" />
 </head>
 <body>
+<ul class="act">
 <?php
     $result = select($_GET["query"]);
 
     foreach ($result as $line)
     {
-        echo "<li>" . $line['username'] . ", " .  $line['title'] . ", " .  $line['description'] . ", " . $line['info'] . "</li>";
+        echo "<li><div class='username'><b>"."~"."&nbsp". $line['username'] ."&nbsp"."~"."</b>"."</div><div class='title'><b> ".  $line['title'] .":". "<button id='button'>Rejoindre</button>"."</b></div><div class='description'> " .  $line['description'] . "</div><div class='info'> " . $line['info'] . "</div></li>";
     }
-?>
+?></ul>
 </body>
 </html>
