21d37b56ba40691d1c681e167a4d973d6051f610
Babouche <bat.perrier@free.fr>
2021-05-23 08:08:01 +0200

    try to repair

--- pages/profil.php
+++ pages/profil.php
@@ -12,55 +12,54 @@
 </form>
 
 <?php
-    include('../config.php');
-    global $db;
-    $id = $_GET["id"];
-    $query = $db->prepare("SELECT * FROM users WHERE id=:id");
-    $query->bindParam("id",$id, PDO::PARAM_STR);
+include('../config.php');
+global $db;
+$id = $_GET["id"];
+$query = $db->prepare("SELECT * FROM users WHERE id=:id");
+$query->bindParam("id",$id, PDO::PARAM_STR);
+$query->execute();
+if ($query->rowCount() == 0) {
+    header('HTTP/1.0 404 Not Found');
+    exit;
+}
+else{
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+    $username = $result["username"];
+    echo 'Username: '. $username. '<br/>';
+    $email = $result["email"];
+    echo 'Email: '. $email. '<br/>';
+    $query = $db->prepare("SELECT * FROM activities WHERE author_id=:author_id");
+    $query->bindParam("author_id",$id, PDO::PARAM_STR);
     $query->execute();
-    if ($query->rowCount() == 0) {
-        header('HTTP/1.0 404 Not Found');
-        exit;
-    }
-    else{
-        $result = $query->fetch(PDO::FETCH_ASSOC);
-        $username = $result["username"];
-        echo 'Username: '. $username. '<br/>';
-        $email = $result["email"];
-        echo 'Email: '. $email. '<br/>';
-        $query = $db->prepare("SELECT * FROM activities WHERE author_id=:author_id");
-        $query->bindParam("author_id",$id, PDO::PARAM_STR);
-        $query->execute();
-        if($query->rowCount() > 0)
-            echo "Activités: <br/>";
-        else
-            echo "Aucune activité";
+    if($query->rowCount() > 0)
+        echo "Activités: <br/>";
+    else
+        echo "Aucune activité";
 //        $result = $query->fetch(PDO::FETCH_ASSOC);
-?>
-<div class="dropdown">
-  <?php
-        $t = $query->rowCount();
-        while($t>0 AND $activity = $query->fetch()){
-            ?><span><i><?php echo $activity["title"] . '<br/>';?></i></span>
-            <div class="dropdown-content">
-                <p><?php echo $activity["description"] . '<br/>';?></p>
-              </div>
-            </div><br/><?php
-            ++$t;
-        }
+    ?>
+    <div class="dropdown">
+    <?php
+    $t = $query->rowCount();
+    while($t>0 AND $activity = $query->fetch()){
+        ?><span><i><?php echo $activity["title"] . '<br/>';?></i></span>
+        <div class="dropdown-content">
+            <p><?php echo $activity["description"] . '<br/>';?></p>
+        </div>
+        </div><br/><?php
+        ++$t;
     }
+}
 ?>
-  
+
 <form> </br><input type="button" onclick="location.href='../pages/activities.php';" value="Ajouter une activité "/></form>
 <form> <input type="button" onclick="location.href='../index.php';" value="Retour au site "/></form>
 
 <?php
-    session_start();
-    if($_SESSION['user_id'] == $id){
-        $userlink = '../pages/user_change.php?id='. $_SESSION['user_id'];
-        echo '<form> <input type=button onclick=location.href=\''.$userlink.'\' value="Modifier le profil "/></form>';
-    }
+session_start();
+if($_SESSION['user_id'] == $id){
+    $userlink = '../pages/user_change.php?id='. $_SESSION['user_id'];
+    echo '<form> <input type=button onclick=location.href=\''.$userlink.'\' value="Modifier le profil "/></form>';
+}
 ?>
 </body>
 </html>
-
